71c89766168d519a935ca980c788bc28de40a54d
Josvin-technology <jventuras@miumg.edu.gt>
2024-11-18 19:15:59 -0600

    Agregue un boton eleminar

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,23 +32,25 @@
             this.PrimerBoton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(290, 177);
+            this.label1.Location = new System.Drawing.Point(326, 221);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(261, 55);
+            this.label1.Size = new System.Drawing.Size(314, 66);
             this.label1.TabIndex = 0;
             this.label1.Text = "Hola Mundo";
             // 
             // PrimerBoton
             // 
-            this.PrimerBoton.Location = new System.Drawing.Point(475, 286);
+            this.PrimerBoton.Location = new System.Drawing.Point(534, 358);
+            this.PrimerBoton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.PrimerBoton.Name = "PrimerBoton";
-            this.PrimerBoton.Size = new System.Drawing.Size(227, 63);
+            this.PrimerBoton.Size = new System.Drawing.Size(255, 79);
             this.PrimerBoton.TabIndex = 1;
             this.PrimerBoton.Text = "Cambiar";
             this.PrimerBoton.UseVisualStyleBackColor = true;
@@ -56,30 +58,42 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(394, 86);
+            this.label2.Location = new System.Drawing.Point(443, 108);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.Size = new System.Drawing.Size(60, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "cambio";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(104, 286);
+            this.button1.Location = new System.Drawing.Point(117, 358);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(239, 62);
+            this.button1.Size = new System.Drawing.Size(269, 78);
             this.button1.TabIndex = 3;
             this.button1.Text = "form Josvin";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(328, 469);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(262, 76);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ELEMINAR";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.PrimerBoton);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -94,6 +108,7 @@
         private System.Windows.Forms.Button PrimerBoton;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
